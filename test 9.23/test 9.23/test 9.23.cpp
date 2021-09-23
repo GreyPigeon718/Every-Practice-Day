5e8808373c4e6be711fa9d06ee4808ce2ca35995
GreyPigeon718 <2604059284@qq.com>
2021-09-23 20:47:39 +0800

    “空格替换”

---
+++ test 9.23/test 9.23/test 9.23.cpp
@@ -0,0 +1,19 @@
+//Ìæ»»¿Õ¸ñ
+//class Solution {
+//public:
+//
+//    string replaceSpace(string s) {
+//        // write code here
+//        string res;
+//        for (int i = 0; i < s.size(); ++i)
+//        {
+//            if (s[i] == ' ')
+//            {
+//                res += "%20";
+//            }
+//            else
+//                res += s[i];
+//        }
+//        return res;
+//    }
+//};
