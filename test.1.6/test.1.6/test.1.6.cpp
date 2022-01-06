e7da385be46a207f292aca788b6225c8a4eee932
unknown <2604059284@qq.com>
2022-01-06 22:57:34 +0800

    搜索旋转排序数组

---
+++ test.1.6/test.1.6/test.1.6.cpp
@@ -0,0 +1,12 @@
+//ËÑË÷Ğı×ªÅÅĞòÊı×é
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            if (nums[i] == target)
+                return i;
+        }
+        return -1;
+    }
+};
