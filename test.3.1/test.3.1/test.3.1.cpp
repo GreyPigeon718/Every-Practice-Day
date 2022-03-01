33bdbdbffeba90ecf24b910cbf45729fc4afe560
unknown <2604059284@qq.com>
2022-03-01 22:49:58 +0800

    1.解码方法2.最长公共子序列

---
+++ test.3.1/test.3.1/test.3.1.cpp
@@ -0,0 +1,38 @@
+//> 解码方法
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n = s.size();
+        vector<int> f(n + 1);
+        f[0] = 1;
+        for (int i = 1; i <= n; ++i) {
+            if (s[i - 1] != '0') {
+                f[i] += f[i - 1];
+            }
+            if (i > 1 && s[i - 2] != '0' && ((s[i - 2] - '0') * 10 + (s[i - 1] - '0') <= 26)) {
+                f[i] += f[i - 2];
+            }
+        }
+        return f[n];
+    }
+};
+
+//> 最长公共子序列
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        vector<vector<int>> dp(text1.size() + 1, vector<int>(text2.size() + 1, 0));
+
+        for (int i = 1; i <= text1.size(); ++i)
+        {
+            for (int j = 1; j <= text2.size(); ++j)
+            {
+                if (text1[i - 1] == text2[j - 1])
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                else
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        return dp[text1.size()][text2.size()];
+    }
+};
