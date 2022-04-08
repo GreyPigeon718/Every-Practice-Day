a9fd0d9b00a33401294a37d490e17b39c51879d1
unknown <2604059284@qq.com>
2022-04-08 23:26:11 +0800

    N叉树的层序遍历

---
+++ test.4.8/test.4.8/test.4.8.cpp
@@ -0,0 +1,24 @@
+//> N 叉树的层序遍历
+class Solution {
+public:
+    vector<vector<int>> levelOrder(Node* root) {
+        queue<Node*> que;
+        if (root != NULL) que.push(root);
+        vector<vector<int>> result;
+        while (!que.empty()) {
+            int size = que.size();
+            vector<int> vec;
+            for (int i = 0; i < size; i++) {
+                Node* node = que.front();
+                que.pop();
+                vec.push_back(node->val);
+                for (int i = 0; i < node->children.size(); i++) { // 将节点孩子加入队列
+                    if (node->children[i]) que.push(node->children[i]);
+                }
+            }
+            result.push_back(vec);
+        }
+        return result;
+
+    }
+};
