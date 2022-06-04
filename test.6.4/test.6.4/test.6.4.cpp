d3bbee938340cadc476dcf8b48c35b85115253ab
unknown <2604059284@qq.com>
2022-06-04 21:43:16 +0800

    1.独特的电子邮件地址

--- test.6.4/test.6.4/test.6.4.cpp
+++ test.6.4/test.6.4/test.6.4.cpp
@@ -1,109 +1,20 @@
-//#include <iostream>
-//
-//using namespace std;
-//
-//class A
-//{
-//public:
-//    void foo()
-//    {
-//        printf("1");
-//    }
-//    virtual void fun()
-//    {
-//        printf("2");
-//    }
-//};
-//class B : public A
-//{
-//public:
-//    void foo()
-//    {
-//        printf("3");
-//    }
-//    void fun()
-//    {
-//        printf("4");
-//    }
-//};
-//int main(void)
-//{
-//    A a;
-//    B b;
-//    A* p = &a;
-//    p->foo();
-//    p->fun();
-//    p = &b;
-//    p->foo();
-//    p->fun();
-//    A* ptr = (A*)&b;
-//    ptr->foo();
-//    ptr->fun();
-//    return 0;
-//}
-//class A
-//{
-//public:
-//    virtual void func(int val = 1)
-//    {
-//        std::cout << "A->" << val << std::endl;
-//    }
-//    virtual void test()
-//    {
-//        func();
-//    }
-//};
-//class B : public A
-//{
-//public:
-//    void func(int val = 0)
-//    {
-//        std::cout << "B->" << val << std::endl;
-//    }
-//};
-//int main(int argc, char* argv[])
-//{
-//    const int& a = 1;
-//    B* p = new B;
-//    p->test();
-//    return 0;
-//}
-//最近公共祖先
-//class LCA {
-//public:
-//    int getLCA(int a, int b) {
-//        // write code here
-//        while (a != b)
-//        {
-//            if (a > b)
-//                a /= 2;
-//            else
-//                b /= 2;
-//        }
-//        return a;
-//    }
-//};
-#include <iostream>
-
-using namespace std;
-
-int main()
-{
-    int x;
-    int max = 0;
-    cin >> x;
-    int count = 0;
-    while (x)
-    {
-        if (x & 1)
-        {
-            count++;
-            max = max > count ? max : count;
+//> 独特的电子邮件地址
+class Solution {
+public:
+    int numUniqueEmails(vector<string>& emails) {
+        unordered_set<string> emailSet;
+        for (auto& email : emails) {
+            string local;
+            for (char c : email) {
+                if (c == '+' || c == '@') {
+                    break;
+                }
+                if (c != '.') {
+                    local += c;
+                }
+            }
+            emailSet.emplace(local + email.substr(email.find('@')));
         }
-        else
-            count = 0;
-        x = x >> 1;
+        return emailSet.size();
     }
-    cout << max << endl;
-    return 0;
-}
+};
