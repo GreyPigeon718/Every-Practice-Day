7fe67b04c9b022296684451fc98d53523104eff3
unknown <2604059284@qq.com>
2022-06-14 22:25:06 +0800

    1.复杂链表的复制

---
+++ test.6.14/test.6.14/test.6.14.cpp
@@ -0,0 +1,19 @@
+
+//> ¸´ÔÓÁ´±íµÄ¸´ÖÆ
+class Solution {
+public:
+    unordered_map<Node*, Node*> cachedNode;
+
+    Node* copyRandomList(Node* head) {
+        if (head == nullptr) {
+            return nullptr;
+        }
+        if (!cachedNode.count(head)) {
+            Node* headNew = new Node(head->val);
+            cachedNode[head] = headNew;
+            headNew->next = copyRandomList(head->next);
+            headNew->random = copyRandomList(head->random);
+        }
+        return cachedNode[head];
+    }
+};
