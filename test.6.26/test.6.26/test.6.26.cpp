31dc1a2a9c2542b2d8becc61aa19f3a0328f057c
unknown <2604059284@qq.com>
2022-06-26 21:57:13 +0800

    1.1～n 整数中 1 出现的次数

---
+++ test.6.26/test.6.26/test.6.26.cpp
@@ -0,0 +1,16 @@
+//> 1～n 整数中 1 出现的次数
+class Solution {
+public:
+    int countDigitOne(int n) {
+        // mulk 表示 10^k
+        // 在下面的代码中，可以发现 k 并没有被直接使用到（都是使用 10^k）
+        // 但为了让代码看起来更加直观，这里保留了 k
+        long long mulk = 1;
+        int ans = 0;
+        for (int k = 0; n >= mulk; ++k) {
+            ans += (n / (mulk * 10)) * mulk + min(max(n % (mulk * 10) - mulk + 1, 0LL), mulk);
+            mulk *= 10;
+        }
+        return ans;
+    }
+};
