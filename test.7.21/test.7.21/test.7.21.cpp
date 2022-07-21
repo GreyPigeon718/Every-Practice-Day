4e7f0558dd6bb49be87ef881b162188d65feb72d
unknown <2604059284@qq.com>
2022-07-21 23:25:10 +0800

    1.最大体积值2.滑动窗口的最大值3.左旋转字符串

--- test.7.21/test.7.21/test.7.21.cpp
+++ test.7.21/test.7.21/test.7.21.cpp
@@ -1,71 +1,88 @@
-//字符串计数
-#include<iostream>
-#include<string>
-#include<vector>
-#include<math.h>
+//> 最大体积值
+
+#include <iostream>
+#include <vector>
 using namespace std;
 
-int main() {
-    //根据题中给出的例子，这个字符串只包含小写字母，不然答案就不应该是56了
-    string s1, s2;
-    int len1, len2;
-    while (cin >> s1 >> s2 >> len1 >> len2) {
-        //只包含小写字母的字符串可以看成26进制的数制
-        //将s1和s2补长到len2长度
-        s1.append(len2 - s1.size(), 'a');
-        s2.append(len2 - s2.size(), (char)('z' + 1));
-        vector<int> array;
-        for (int i = 0; i < len2; i++) {
-            array.push_back(s2[i] - s1[i]);
-        }
-        int result = 0;
-        for (int i = len1; i <= len2; i++) {
-            for (int k = 0; k < i; k++) {
-                result += array[k] * pow(26, i - 1 - k);
+
+
+bool isPrime(long long x)
+{
+    for (long i = 2; i <= sqrt(x); ++i)
+    {
+        if (x % i == 0)
+            return false;
+    }
+    return true;
+}
+long long getMaxVolume(long long n) {
+    // write code here
+    vector<long long> res;
+    long long maxSum = 0;
+    for (long long i = 2; i <= n; ++i)
+    {
+        if (isPrime(i) == true)
+            res.emplace_back(i);
+    }
+    for (int i = 0; i < res.size(); ++i)
+    {
+        for (int j = i; j < res.size(); ++j)
+        {
+            for (int k = j; k < res.size(); ++k)
+            {
+                if (res[i] + res[j] + res[k] == n)
+                {
+                    long long num = res[i] * res[j] * res[k];
+                    maxSum = num > maxSum ? num : maxSum;
+                }
             }
         }
-        //所有字符串最后都不包含是s2自身，所以最后要减1；
-        cout << result - 1 << endl;
     }
-    return 0;
+    for (int i = 0; i < res.size(); ++i)
+        cout << res[i] << " ";
+    cout << endl;
+    return maxSum;
 }
 
-//两个数组交集
+int main()
+{
+    int x;
+    while (cin >> x)
+        cout << getMaxVolume(x) << endl;
+
+}
+
+
+//> 滑动窗口的最大值
 class Solution {
 public:
-    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        if (nums1.size() > nums2.size())
-            return intersect(nums2, nums1);
-        unordered_map <int, int> m;
-        for (int num : nums1)
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int left = 0;
+        int right = k - 1;
+        vector<int> res;
+        while (right < nums.size())
         {
-            ++m[num];
-        }
-        vector<int> both;
-        for (int num : nums2)
-        {
-            if (m.count(num))
+            int maxValue = nums[left];
+            for (int i = left; i <= right; ++i)
             {
-                both.push_back(num);
-                --m[num];
-                if (m[num] == 0)
-                    m.erase(num);
+                maxValue = max(maxValue, nums[i]);
             }
+            res.emplace_back(maxValue);
+            left++;
+            right++;
         }
-        return both;
-    }
-};
-//买卖股票最好的时机
-class Solution {
-public:
-    int maxProfit(vector<int>& prices) {
-        int inf = 1e9;
-        int minprice = inf, maxprofit = 0;
-        for (int price : prices) {
-            maxprofit = max(maxprofit, price - minprice);
-            minprice = min(price, minprice);
-        }
-        return maxprofit;
+        return res;
     }
 };
 
+
+//> 左旋转字符串
+class Solution {
+public:
+    string reverseLeftWords(string s, int n) {
+        reverse(s.begin(), s.begin() + n);
+        reverse(s.begin() + n, s.end());
+        reverse(s.begin(), s.end());
+        return s;
+    }
+};
