1c0abb309702e8a39014da39adf62b046a60deb6
unknown <2604059284@qq.com>
2022-09-21 21:53:28 +0800

    1.颜色分类

---
+++ test.9.21/test.9.21/test.9.21.cpp
@@ -0,0 +1,27 @@
+// —’…´∑÷¿‡
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int p = 0;
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            if (nums[i] == 0)
+            {
+                int temp = nums[p];
+                nums[p] = nums[i];
+                nums[i] = temp;
+                p++;
+            }
+        }
+        for (int j = p; j < nums.size(); ++j)
+        {
+            if (nums[j] == 1)
+            {
+                int temp = nums[p];
+                nums[p] = nums[j];
+                nums[j] = temp;
+                p++;
+            }
+        }
+    }
+};
